fbb640f1f41cde5087561bcf2a2947e2e57f9802
rshul <romander@ukr.net>
2019-03-06 00:35:58 +0200

    solution created

---
+++ SRR.UIP.HW2.ConditionsArraysLoops/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRR.UIP.HW2.ConditionsArraysLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
