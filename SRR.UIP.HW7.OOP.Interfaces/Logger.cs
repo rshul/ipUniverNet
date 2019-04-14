3bf929ac0cdda93e9044ffe218c04d45147f7330
rshul <romander@ukr.net>
2019-04-14 12:07:36 +0300

    Logger implementation work in class

---
+++ SRR.UIP.HW7.OOP.Interfaces/Logger.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRR.UIP.HW7.OOP.Interfaces
+{
+    class Logger : ILogger
+    {
+        private int _logLevel;
+        public int LogLevel
+        {
+            get { return this._logLevel; }
+            set
+            {
+                if (value < 1 || value > 5)
+                {
+                    return;
+                }
+                this._logLevel = value;
+            }
+        }
+        public List<ILogStorage> LogStorages { get; } = new List<ILogStorage>();
+        public void Info(string message)
+        {
+            WriteToStorages(1, Program.GetCurrentMethodName(), message);
+        }
+
+        public void Debug(string message)
+        {
+            WriteToStorages(2, Program.GetCurrentMethodName(), message);
+
+
+        }
+
+        public void Error(string message)
+        {
+            WriteToStorages(4, Program.GetCurrentMethodName(), message);
+        }
+
+        public void Fatal(string message)
+        {
+            WriteToStorages(5, Program.GetCurrentMethodName(), message);
+        }
+
+        public void Warn(string message)
+        {
+            WriteToStorages(3, Program.GetCurrentMethodName(), message);
+        }
+        public void WriteToStorages(int logLevelInt, string loglevel, string message)
+        {
+            if (logLevelInt >= this.LogLevel)
+            {
+                foreach (var storage in LogStorages)
+                {
+                    storage.Write($"{loglevel}{DateTime.Now}{message}");
+                }
+            }
+
+        }
+    }
+}
+
---
+++ SRR.UIP.HW7.OOP.Interfaces/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRR.UIP.HW7.OOP.Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = new Logger();
+
+            logger.LogStorages.Add(new ConsoleLogStorage());
+            logger.LogStorages.Add(new FileLogStorage());
+            logger.Info("user loged out");
+            logger.LogStorages.RemoveAt(1);
+            logger.Info("stringinfo");
+            logger.Debug("stringdebug");
+            logger.Warn("stringwarn");
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static string GetCurrentMethodName()
+        {
+            StackFrame sf = new StackTrace().GetFrame(1);
+            return sf.GetMethod().Name;
+        }
+
+    }
+}
