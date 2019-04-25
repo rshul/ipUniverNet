ff43ddbce24c1cd274aebebeb21cfd1d105edcd0
rshul <romander@ukr.net>
2019-04-25 10:57:47 +0300

    added get points functionality to UIConsoleInteractor

--- SRR.UIP.HW8.LandCalculator.UIConsole/UIConsoleInteractor.cs
+++ SRR.UIP.HW8.LandCalculator.UIConsole/UIConsoleInteractor.cs
@@ -1,4 +1,5 @@
-﻿using SRR.UIP.HW8.LandCalculator.Shared.Interfaces;
+﻿using SRR.UIP.HW8.LandCalculator.Shared;
+using SRR.UIP.HW8.LandCalculator.Shared.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -19,16 +20,144 @@ namespace SRR.UIP.HW8.LandCalculator.UIConsole
 
         internal void Start()
         {
-            List<Point> points = GetPoints();
+            List<Point> points = GetPointsTest();
 
             int landArea = LandAreaCalculator.CalculateLandArea(points);
 
             Console.WriteLine($"Result = {landArea}");
         }
 
-        private List<Point> GetPoints()
+        private List<Point> GetPointsTest()
         {
-            return null;
+            List<Point> setOfPoints;
+
+            int choice = 0;
+            switch (choice)
+            {
+                case 0:
+                    setOfPoints = new List<Point>
+                    {
+                        new Point(0,0),
+                        new Point(1,1),
+                        new Point(2,4),
+                        new Point(3,1),
+                        new Point(4,0)
+                    };
+                    break;
+                case 1:
+                    setOfPoints = new List<Point>
+                    {
+                        new Point(0,0),
+                        new Point(1,1)
+                    };
+                    break;
+                case 2://crossed
+                    setOfPoints = new List<Point>
+                    {
+                        new Point(0,0),
+                        new Point(1,1),
+                        new Point(2,4),
+                        new Point(3,1),
+                        new Point(0,4)
+                    };
+                    break;
+                case 3://same point
+                    setOfPoints = new List<Point>
+                    {
+                        new Point(0,0),
+                        new Point(1,1),
+                        new Point(1,1),
+                        new Point(3,1),
+                        new Point(4,0)
+                    };
+                    break;
+                default:
+                    setOfPoints = new List<Point>
+                    {
+                        new Point(0,0),
+                        new Point(0,1),
+                        new Point(1,1),
+                        new Point(1,0),
+                    };
+                    break;
+            }
+            return setOfPoints;
+        }
+
+        private List<Point> GetPoints(int PointsNumber)
+        {
+            if (PointsNumber <= 0 || PointsNumber > 20)
+            {
+                StaticInjector.Logger.Warn("Number of points must be  0 < x <=20");
+                return null;
+            }
+            Point tempPoint;
+            List<Point> tempListOfPoints = new List<Point>();
+            int pointsCounter = 0;
+            while (pointsCounter < PointsNumber)
+            {
+                tempPoint = GetPoint(pointsCounter);
+                bool isInputValid = IsPointValid(tempListOfPoints, tempPoint);
+                if (!isInputValid)
+                {
+                    continue;
+                }
+                tempListOfPoints.Add(tempPoint);
+                StaticInjector.Logger.Info($"Point {pointsCounter} with value ({tempPoint.X},{tempPoint.Y}) added");
+                pointsCounter++;
+
+            }
+
+            return tempListOfPoints;
+        }
+
+        private bool IsPointValid(List<Point> pointsCollection, Point takenPoint)
+        {
+            if (pointsCollection.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                foreach (var point in pointsCollection)
+                {
+                    if (takenPoint.Equals(point))
+                    {
+                        StaticInjector.Logger.Warn("Points can not be the same");
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+        }
+
+        static Point GetPoint(int pointNumber)
+        {
+            int xCoord;
+            int yCoord;
+
+            Console.WriteLine($"Enter coordinates of {pointNumber} point");
+            InputCoordinate("X", out xCoord);
+            InputCoordinate("Y", out yCoord);
+            StaticInjector.Logger.Info($"Point {pointNumber} created, ({xCoord},{yCoord})");
+
+            return new Point(xCoord, yCoord);
+        }
+
+        private static void InputCoordinate(string coordinateName, out int coordinate)
+        {
+            bool isParsedOK = false;
+            do
+            {
+                Console.Write($"{coordinateName} => ");
+                string enteredStringNumber = Console.ReadLine();
+                isParsedOK = int.TryParse(enteredStringNumber, out coordinate);
+                if (!isParsedOK)
+                {
+                    StaticInjector.Logger.Error($"Not valid value of {coordinateName}");
+                }
+            } while (!isParsedOK);
         }
     }
 }
